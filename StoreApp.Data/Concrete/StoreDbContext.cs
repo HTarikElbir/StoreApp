84be2803cab15aae2132f182ecc6b6cac429422e
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-14 23:01:39 +0300

    feat: Add dbcontext reference for connection, StoreApp.Web to StoreApp.Data layer

---
+++ StoreApp.Data/Concrete/StoreDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Data.Entities;
+
+namespace StoreApp.Data.Concrete;
+
+public class StoreDbContext: DbContext
+{
+    public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
---
+++ StoreApp.Data/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace StoreApp.Data.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public string Category { get; set; } = string.Empty;
+}
