ec6457751cb825fb19db0e825e8fc8fcea80ad46
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-16 21:45:37 +0300

    feat: Add Pagination Tag Helper for Index Page

---
+++ StoreApp.Web/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.TagHelpers;
+
+[HtmlTargetElement("div", Attributes = "page-model")]
+public class PageLinkTagHelper: TagHelper
+{
+    private IUrlHelperFactory _urlHelperFactory;
+
+    public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+    {
+        _urlHelperFactory = helperFactory;
+    }
+    
+    [ViewContext]
+    public ViewContext? ViewContext { get;}
+    public PageInfo? PageModel { get;}
+    public string? PageAction { get; set; }
+    
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if(ViewContext != null && PageModel != null)
+        {
+            IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder div = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder link = new TagBuilder("a");
+                link.Attributes["href"] = urlHelper.Action(PageAction, new { page = i});
+                link.InnerHtml.Append(i.ToString());
+                div.InnerHtml.AppendHtml(link);
+            }
+            output.Content.AppendHtml(div);
+        }
+    }
+}
