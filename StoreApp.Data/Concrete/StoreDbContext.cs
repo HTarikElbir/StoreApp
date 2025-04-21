707ac7427d0dcc166092177cf9a1a9a97bf4c840
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-21 21:41:24 +0300

    feat: Add Order Detail Page

--- StoreApp.Data/Concrete/StoreDbContext.cs
+++ StoreApp.Data/Concrete/StoreDbContext.cs
@@ -12,6 +12,7 @@ public class StoreDbContext: DbContext
 
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Order> Orders { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
